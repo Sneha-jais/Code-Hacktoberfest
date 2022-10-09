62a9a6f5c9004fbb04c69e8d8ae0eb9517dd4b13
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-10-10 00:01:10 +0530

    Create Rec_linear_search.cpp

---
+++ Data Structure(Basic)/6.Searching/Rec_linear_search.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int linear_search(int arr[],int n,int key,int start){ 
+ if(start<=n-1){
+ if(arr[start]==key)
+ return start;
+ else
+ return linear_search(arr,n,key,start+1);
+ }
+ return -1;
+}
+int main()
+{
+int n,key;
+cout<<"Enter number of elements in array:\n";
+cin>>n;
+cout<<"Enter elements of the array:\n";
+int arr[n];
+for(int i=0;i<n;i++){
+ cin>>arr[i];
+}
+cout<<"Enter the element which you want to search:\n";
+cin>>key;
+int ans=linear_search(arr,n,key,0);
+cout<<"The index of desired element is: "<<ans<<endl;
+ return 0;
+}
