e7db5d1f6b70ec9d2a726be1bb2fba614d41751b
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-12 00:09:37 +0530

    Update CountTheNumberOfPossibleTriangles.cpp

--- Data Structure(Basic)/2.Arrays/CountTheNumberOfPossibleTriangles.cpp
+++ Data Structure(Basic)/2.Arrays/CountTheNumberOfPossibleTriangles.cpp
@@ -7,3 +7,43 @@ Explanation: There are three triangles
 possible {3, 4, 6}, {4, 6, 7} and {3, 6, 7}. 
 Note that {3, 4, 7} is not a possible triangle.  
 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findNumberOfTriangles(int arr[], int n)
+{
+
+	int count = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+
+			for (int k = j + 1; k < n; k++)
+
+				if (arr[i] + arr[j] > arr[k]
+					&& arr[i] + arr[k] > arr[j]
+					&& arr[k] + arr[j] > arr[i])
+					count++;
+		}
+	}
+	return count;
+}
+
+
+int main()
+{
+
+int size;
+  cin>>size;
+  int arr[size];
+  for(int i=0;i<size;i++){
+    cin>>arr[i];
+  }
+  
+	cout << "Total number of triangles possible is "
+		<< findNumberOfTriangles(arr, size);
+	return 0;
+}
+
