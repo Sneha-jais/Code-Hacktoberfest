e26954289d55896464680083ba872c9cb9eca9d5
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-12-12 00:04:00 +0530

    Create CountTheNumberOfPossibleTriangles.cpp

---
+++ Data Structure(Basic)/2.Arrays/CountTheNumberOfPossibleTriangles.cpp
@@ -0,0 +1,9 @@
+/*
+Given an unsorted array of positive integers, find the number of triangles that can be formed with three different array elements as three sides of triangles. 
+For a triangle to be possible from 3 values, the sum of any of the two values (or sides) must be greater than the third value (or third side). 
+Input: arr= {4, 6, 3, 7}
+Output: 3
+Explanation: There are three triangles 
+possible {3, 4, 6}, {4, 6, 7} and {3, 6, 7}. 
+Note that {3, 4, 7} is not a possible triangle.  
+*/
